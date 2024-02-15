e6304a8dc36e433a9e670b6bdbd01ed102cb2fc9
Dylan Olivro <dylanolivro94@gmail.com>
2024-02-15 10:18:42 +0100

    add create methode with provider and controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { CarsService } from "./cars.service";
+import { CreateCarDto } from "./dto/create-car.dto";
 
-@Controller('cars')
-export class CarsController {}
+@Controller("cars")
+export class CarsController {
+    constructor(private readonly carService: CarsService) {}
+
+    @Post()
+    async create(@Body() createCarDto: CreateCarDto) {
+        await this.carService.create(createCarDto);
+    }
+}
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,4 @@
-export class CreateCarDta {
+export class CreateCarDto {
     readonly brand: string;
     readonly price: string;
     readonly description: string;
